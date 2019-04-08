95f0fca001dc7ab07a58eaf63165d72639a7a242
Kate <you@example.com>
2019-04-07 17:04:55 -0700

    bootstrap, html, java, css, html start, firebase added.

---
+++ app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDP9gMSt9dwxl9m17-5-Ggq_SyIw7O-4io",
+    authDomain: "trainshomework-c66df.firebaseapp.com",
+    databaseURL: "https://trainshomework-c66df.firebaseio.com",
+    projectId: "trainshomework-c66df",
+    storageBucket: "trainshomework-c66df.appspot.com",
+    messagingSenderId: "836267274826"
+  };
+
+firebase.initializeApp(config);
+  
