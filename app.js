393e11088c33474ebf9b6daac9e41bdfd4835f5d
Kate <you@example.com>
2019-04-07 17:52:35 -0700

    jQuery added, database added.

--- app.js
+++ app.js
@@ -8,4 +8,31 @@ var config = {
   };
 
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var trainDestination = "";
+var trainTime = 0;
+var frequency = 0;
+
+// Capture Button Click
+$("#button").on("click", function(event) {
+    event.preventDefault();
+
+    // Grabbed values from text-boxes
+    trainName = $("#trainName-input").val().trim();
+    trainDestination = $("#trainDestination-input").val().trim();
+    trainTime = $("trainTime-input").val().trim();
+    frequency = $("#trainFrequency-input").val().trim();
+
+    // Code for "Setting values in the database"
+    database.ref().set({
+      Name: trainNAme,
+      Destination: trainDestination,
+      Time: trainTime,
+      Frequencey: frequency,
+    });
+
+  });
   
