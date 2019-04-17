9865f860e847ca1fab62032fa4c18fb245af4f1e
Kate <you@example.com>
2019-04-16 19:29:51 -0700

    changes

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  // Create two variable with the names of the months and days in an array
-  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
-  var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  // // Create two variable with the names of the months and days in an array
+  // var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+  // var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
   
-  // Create a newDate() object
-  var newDate = new Date();
-  // Extract the current date from Date object
-  newDate.setDate(newDate.getDate());
-  // Output the day, date, month and year   
-  $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
+  // // Create a newDate() object
+  // var newDate = new Date();
+  // // Extract the current date from Date object
+  // newDate.setDate(newDate.getDate());
+  // // Output the day, date, month and year   
+  // $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
   
   setInterval( function() {
     // Create a newDate() object and extract the seconds of the current time on the visitor's
@@ -31,7 +31,7 @@ $(document).ready(function() {
     $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
       }, 1000);	
   });
-  
+
   var seedInfo = [{
   "trainName": "Hogwarts Express",
   "trainDestination": "Hogwarts",
